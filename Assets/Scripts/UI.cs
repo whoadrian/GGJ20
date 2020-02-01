7d4c82e0b8a63fa7d808bb33a6e3189b1c53a28b
Adrian Cristea <whoa.adrian@gmail.com>
2020-02-01 13:12:48 +0100

    ui

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+    private GameSystem system;
+    
+    public void Play() {
+        if (!CheckSanity()) {
+            return;
+        }
+        
+        system.Command(Command.PLAY);
+    }
+
+    public void Pause() {
+        if (!CheckSanity()) {
+            return;
+        }
+        
+        system.Command(Command.PAUSE);
+    }
+
+    public void Stop() {
+        if (!CheckSanity()) {
+            return;
+        }
+        
+        system.Command(Command.STOP);
+    }
+
+    private bool CheckSanity() {
+        if (system == null) {
+            system = GameObject.FindObjectOfType<GameSystem>();
+        }
+
+        if (system != null && Application.isPlaying) {
+            return true;
+        }
+
+        return false;
+    }
+}
